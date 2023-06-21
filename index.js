16c087c72648374a6a44a2830b861f031e5b60a9
Sangwoo Yu <76999175+k7nsuy@users.noreply.github.com>
2023-06-21 17:26:09 +0900

    3xx status
    응답이 3xx 리다이렉션인 경우

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
  import fetch from "node-fetch"; // HTTP 요청을 보내기 위해 사용
+ 
+ async function isSpam(content, spamLinkDomains, redirectionDepth) {
+   // 만약 redirection depth가 0 이면 스팸이 아닙니다.
+   if (redirectionDepth === 0) { 
+     return false; 
+    }
+    
+    
+    const response = await fetch(content); // content URL로 HTTP 요청을 보냅니다.
+    const finalUrl = response.url; // 리디렉션을 따라 이동한 후의 최종 URL을 가져옵니다.
+    
+    // 최종 URL이 스팸 링크 도메인을 포함하는지 확인
+    const isSpamLink = spamLinkDomains.some((domain) => finalUrl.includes(domain)); 
+    
+    if (isSpamLink) {
+      return true; // 스팸 링크라면 true를 반환
+    }
 
-async function isSpam(content, spamLinkDomains, redirectionDepth) {
-  // 만약 redirection depth가 0 이면 스팸이 아닙니다.
-  if (redirectionDepth === 0) { 
-    return false; 
+  // 응답이 3xx 리다이렉션 인 경우
+  if (response.status >= 300 && response.status < 400) {
+    // response 객체의 header 속성에서 'location' 헤더를 가져와 리다이렉션 위치 확인
+    const locationHeader = response.headers.get('location');
+    if (locationHeader) { // 만약 locationHeader가 존재하면,
+      // isSpam 함수를 통해 Spam check 확인 및 rediectionDepth 1 제거.
+      // 3xx 리다이렉션이 없을 때 까지 해당 조건 반복
+      return await isSpam(locationHeader, spamLinkDomains, redirectionDepth - 1);
+    }
   }
-
-  const response = await fetch(content); // content URL로 HTTP 요청을 보냅니다.
-  const finalUrl = response.url; // 리디렉션을 따라 이동한 후의 최종 URL을 가져옵니다.
-
-  // 최종 URL이 스팸 링크 도메인을 포함하는지 확인
-  const isSpamLink = spamLinkDomains.some((domain) => finalUrl.includes(domain)); 
-
-  if (isSpamLink) {
-    return true; // 스팸 링크라면 true를 반환
-  }
-
+  
   return false; // 어떤 스팸 조건에도 해당하지 않으면 스팸이 아닙니다.
 }
+
