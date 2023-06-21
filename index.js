0d73de42fdfe36415ad8e12434f36f5c3bca638a
Sangwoo Yu <76999175+k7nsuy@users.noreply.github.com>
2023-06-21 17:42:00 +0900

    a tag check
    a tag 여부확인

--- index.js
+++ index.js
@@ -2,20 +2,18 @@
  
  async function isSpam(content, spamLinkDomains, redirectionDepth) {
    // 만약 redirection depth가 0 이면 스팸이 아닙니다.
-   if (redirectionDepth === 0) { 
+  if (redirectionDepth === 0) { 
      return false; 
-    }
+  }
     
-    
-    const response = await fetch(content); // content URL로 HTTP 요청을 보냅니다.
-    const finalUrl = response.url; // 리디렉션을 따라 이동한 후의 최종 URL을 가져옵니다.
-    
-    // 최종 URL이 스팸 링크 도메인을 포함하는지 확인
-    const isSpamLink = spamLinkDomains.some((domain) => finalUrl.includes(domain)); 
-    
-    if (isSpamLink) {
-      return true; // 스팸 링크라면 true를 반환
-    }
+  const response = await fetch(content); // content URL로 HTTP 요청을 보냅니다.
+  const finalUrl = response.url; // 리디렉션을 따라 이동한 후의 최종 URL을 가져옵니다.
+  
+  // 최종 URL이 스팸 링크 도메인을 포함하는지 확인
+  const isSpamLink = spamLinkDomains.some((domain) => finalUrl.includes(domain)); 
+  if (isSpamLink) {
+    return true; // 스팸 링크라면 true를 반환
+  }
 
   // 응답이 3xx 리다이렉션 인 경우
   if (response.status >= 300 && response.status < 400) {
@@ -28,6 +26,13 @@
     }
   }
   
+  // HTML body에서 <a href="link"></a> 태그를 찾아 스팸 여부를 확인
+  const html = await response.text();
+  // a tag가 문자열에 포함될 시 true 반환
+  const isSpamHtml = html.includes(`<a href="${link}"></a>`);
+  if (isSpamHtml) {
+    return isSpamHtml;
+  }
+  
   return false; // 어떤 스팸 조건에도 해당하지 않으면 스팸이 아닙니다.
 }
-
